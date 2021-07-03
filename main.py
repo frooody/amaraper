1cdfb9392a5fe5c763c09a09fbd456398b3c9dc7
froody <47475328+frooody@users.noreply.github.com>
2021-07-03 22:37:01 +0500

    Update main.py
    Making script runs headless

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
 
 options = webdriver.ChromeOptions()
-options.headless = False
+options.headless = True
 options.add_argument(f'user-agent={user_agent}')
 options.add_argument("--window-size=1920,1080")
 options.add_argument('--ignore-certificate-errors')
